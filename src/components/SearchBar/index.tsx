b2820d2df98a17e12641b024fe93165076f56b31
philgoeschl <contact@philgoeschl.com>
2025-05-02 13:11:31 +0200

    base styling for search bar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from "./index.module.scss";
 
 interface SearchBarProps {
   onSearch: (query: string) => void;
@@ -7,20 +8,25 @@ interface SearchBarProps {
 export default function SearchBar({ onSearch }: SearchBarProps) {
   const [query, setQuery] = useState("");
 
+  // TODO: store last query in session storage -> browser navigation
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     onSearch(query);
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <input
           type="text"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           placeholder="Search recipes..."
+          className={styles.input}
         />
-        <button type="submit">Search</button>
+        <button type="submit" className={styles.button}>
+          Search
+        </button>
       </form>
     </div>
   );
