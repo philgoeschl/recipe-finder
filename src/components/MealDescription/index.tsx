78084594975ee13484b61488b7a8f61fe1080ebc
philgoeschl <contact@philgoeschl.com>
2025-05-18 15:02:04 +0200

    display buttons according to isLikedMeal state

--- src/components/MealDescription/index.tsx
+++ src/components/MealDescription/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styles from "./index.module.scss";
 
 interface Meal {
@@ -11,49 +12,62 @@ interface MealProps {
   meal: Meal;
 }
 
-const addToMyRecipes = (meal: Meal) => {
-  const myRecipesJSON = localStorage.getItem("myRecipes");
-  const myRecipes: Meal[] = myRecipesJSON ? JSON.parse(myRecipesJSON) : [];
-
-  if (!isAlreadyLiked(meal)) {
-    myRecipes.push(meal);
-    localStorage.setItem("myRecipes", JSON.stringify(myRecipes));
-  }
-};
-
-const isAlreadyLiked = (meal: Meal) => {
-  const myRecipesJSON = localStorage.getItem("myRecipes");
-  const myRecipes: Meal[] = myRecipesJSON ? JSON.parse(myRecipesJSON) : [];
-  return myRecipes.some((m) => m.idMeal === meal.idMeal);
-};
-
-const removeFromMyRecipes = (meal: Meal) => {
-  const myRecipesJSON = localStorage.getItem("myRecipes");
-  const myRecipes: Meal[] = myRecipesJSON ? JSON.parse(myRecipesJSON) : [];
-
-  if (isAlreadyLiked(meal)) {
-    const myNewRecipes = myRecipes.filter((m) => m.idMeal !== meal.idMeal);
-    localStorage.setItem("myRecipes", JSON.stringify(myNewRecipes));
-  }
-};
-
 export default function MealDescription({ meal }: MealProps) {
+  const isMealLiked = (meal: Meal) => {
+    const myRecipesJSON = localStorage.getItem("myRecipes");
+    const myRecipes: Meal[] = myRecipesJSON ? JSON.parse(myRecipesJSON) : [];
+    return myRecipes.some((m) => m.idMeal === meal.idMeal);
+  };
+
+  const addToMyRecipes = (meal: Meal) => {
+    const myRecipesJSON = localStorage.getItem("myRecipes");
+    const myRecipes: Meal[] = myRecipesJSON ? JSON.parse(myRecipesJSON) : [];
+
+    if (!isMealLiked(meal)) {
+      myRecipes.push(meal);
+      localStorage.setItem("myRecipes", JSON.stringify(myRecipes));
+    }
+  };
+
+  const removeFromMyRecipes = (meal: Meal) => {
+    const myRecipesJSON = localStorage.getItem("myRecipes");
+    const myRecipes: Meal[] = myRecipesJSON ? JSON.parse(myRecipesJSON) : [];
+
+    if (isMealLiked(meal)) {
+      const myNewRecipes = myRecipes.filter((m) => m.idMeal !== meal.idMeal);
+      localStorage.setItem("myRecipes", JSON.stringify(myNewRecipes));
+    }
+  };
+  const [isAlreadyLiked, setIsAlreadyLiked] = useState<boolean>(
+    isMealLiked(meal)
+  );
+
+  useEffect(() => {
+    setIsAlreadyLiked(isMealLiked(meal));
+  }, [isAlreadyLiked, meal]);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.headline}>{meal.strMeal}</h1>
       <p className={styles.description}>{meal.strInstructions}</p>
       <div className={styles.actions}>
-        {!isAlreadyLiked(meal) ? (
+        {!isAlreadyLiked ? (
           <button
             className={styles.button}
-            onClick={() => addToMyRecipes(meal)}
+            onClick={() => {
+              addToMyRecipes(meal);
+              setIsAlreadyLiked(true);
+            }}
           >
             Add to MyRecipes
           </button>
         ) : (
           <button
             className={styles.button}
-            onClick={() => removeFromMyRecipes(meal)}
+            onClick={() => {
+              removeFromMyRecipes(meal);
+              setIsAlreadyLiked(false);
+            }}
           >
             Remove from MyRecipes
           </button>
